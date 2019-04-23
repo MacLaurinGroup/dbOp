22e0a10ff3e4b738e2fd278241ed65ebb722bb9e
Alan Williamson <alan@maclaurin-group.com>
2019-04-23 14:26:16 -0400

    Added in parameter sub

--- classSQLFileRunner.js
+++ classSQLFileRunner.js
@@ -7,13 +7,15 @@
 
 const fs = require("fs");
 const readline = require("readline");
+const Mustache = require("mustache");
 
 class classSQLFileRunner {
 
   constructor(_dbConn, _options) {
     this.dbConn = _dbConn;
     this.options = (typeof _options != "undefined") ? _options : {};
-    this.options.delimiter = (typeof this.options.delimiter != "undefined") ? this.options.delimiter : "";
+    this.delimiter = (typeof this.options.delimiter != "undefined") ? this.options.delimiter : "";
+    delete this.options.delimiter;
   }
 
   async doFile(filename) {
@@ -31,14 +33,13 @@ class classSQLFileRunner {
       rl.on("line", function(chunk) {
         let line = chunk.toString("ascii");
 
-        if (classThis.options.delimiter == "per-line") {
+        if (classThis.delimiter == "per-line") {
           classThis.__executeStatement(line);
         } else {
-          if (line.trim() == classThis.options.delimiter || (classThis.options.delimiter.length == 0 && line.length == 0)) {
+          if (line.trim() == classThis.delimiter || (classThis.delimiter.length == 0 && line.length == 0)) {
             if (stmtBlock != "") {
               statements.push(stmtBlock);
             }
-            //classThis.__executeStatement(stmtBlock);
             stmtBlock = "";
           } else {
             stmtBlock += line + "\r\n";
@@ -66,6 +67,9 @@ class classSQLFileRunner {
   async __executeStatement(stmt) {
     if (stmt.trim() == "")
       return;
+
+    if ( Object.keys(this.options).length > 0 )
+      stmt = Mustache.render( stmt, this.options );
 
     await this.dbConn.query(stmt);
   }
